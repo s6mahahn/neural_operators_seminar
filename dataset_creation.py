6748dd5e64a961562b4e23843d0c2637b1c34f0b
Aleksei Zhuravlev <s94zalek@uni-bonn.de>
2024-02-27 22:32:45 +0100

    added logging to tensorboard; performed experiments, no success

--- dataset_creation.py
+++ dataset_creation.py
@@ -7,6 +7,7 @@ import pickle
 import helper
 
 # Given by tasks some we can play around with
+
 NUM_TRAIN_SAMPLES = 1000
 NUM_VAL_SAMPLES = 200
 NUM_TEST_SAMPLES = 200
@@ -15,16 +16,19 @@ NORMAL_DIST_DOMAIN = [0, 1]
 GRID_START = 0
 GRID_END = 1
 MAX_DEGREE = 4
-NUM_POINTS = 40
+
+NUM_POINTS = 10
 
 
 def sample_points(function):
     x = np.linspace(GRID_START, GRID_END, NUM_POINTS)
+    # sample instead
+    # x = np.random.uniform(GRID_START, GRID_END, NUM_POINTS)
     y = function(x)
     return x, y
 
 
-def generate_coeff(method="uniform"):
+def generate_coeff(method="normal"):
     coeff = []
     for i in range(MAX_DEGREE + 1):
         if method == "uniform":
@@ -77,14 +81,15 @@ def write_to_csv(name, path, data, split):
         writer = csv.writer(f)
         writer.writerows(data)
 
+if __name__ == "__main__":
+    # Create datasets
+    train_set = create_datapoints(NUM_TRAIN_SAMPLES)
+    val_set = create_datapoints(NUM_VAL_SAMPLES)
+    test_set = create_datapoints(NUM_TEST_SAMPLES)
+    datasets_path = "datasets"
+    name = "first dataset"
 
-train_set = create_datapoints(NUM_TRAIN_SAMPLES)
-val_set = create_datapoints(NUM_VAL_SAMPLES)
-test_set = create_datapoints(NUM_TEST_SAMPLES)
-datasets_path = "datasets"
-name = "first dataset"
-
-write_to_pickle(name, datasets_path, train_set, "train")
-write_to_pickle(name, datasets_path, val_set, "val")
-write_to_pickle(name, datasets_path, test_set, "test")
+    write_to_csv(name, datasets_path, train_set, "train")
+    write_to_csv(name, datasets_path, val_set, "val")
+    write_to_csv(name, datasets_path, test_set, "test")
 
