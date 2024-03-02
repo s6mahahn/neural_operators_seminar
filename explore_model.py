63e93ad99af13306acaff2d3b5f4862df7aa1acd
Ccongggg <zhaocongzhaocong0111@gmail.com>
2024-03-02 15:32:28 +0100

    Run on GPU or CPU depending on availability

--- explore_model.py
+++ explore_model.py
@@ -1,6 +1,6 @@
 import numpy as np
 import torch
-
+import torch.cuda
 import dataset_creation
 import helper
 from model import IntegralModel
@@ -38,7 +38,7 @@ def test_other_fun(trained_model, fun, antiderivative, plot_together, fun_name, 
     y = fun[1]
     pred_antiderivative = []
     for i in x:
-        res = trained_model(torch.tensor(np.append(y, i)))
+        res = trained_model(torch.tensor(np.append(y, i)).to(trained_model.device))
         pred_antiderivative.append(res.item())
     #helper.plot_function_by_sampled_points(fun[0], fun[1], title=fun_name, save_file="gen_img/"+fun_name)
     helper.plot_comparison(fun[0], fun[1], true_antiderivative[0],
@@ -48,19 +48,24 @@ def test_other_fun(trained_model, fun, antiderivative, plot_together, fun_name, 
                            pred_antiderivative, title=f"antiderivative of {fun_name}: true vs pred",
                            plot_together=plot_together, save_file="gen_img/"+fun_name+"_int")
 
-trained_model = IntegralModel.load_from_checkpoint("tb_logs/my_model/version_0/checkpoints/epoch=9999-step=630000.ckpt")
-trained_model.eval()
 
-test_other_fun(trained_model, np.cos, np.sin, False, "cos", [0, 1])
-test_other_fun(trained_model, sigmoid, anti_derivative_sigmoid, False, "sigmoid", [0, 1])
-test_other_fun(trained_model, np.exp, np.exp, False, "exp", [0, 1])
+if __name__ == '__main__':
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    trained_model = IntegralModel.load_from_checkpoint("tb_logs/my_model/version_12/checkpoints/epoch=9999-step=160000.ckpt")
+    trained_model.to(device)
+    trained_model.eval()
+
+    test_other_fun(trained_model, np.cos, np.sin, False, "cos", [0, 1])
+    test_other_fun(trained_model, sigmoid, anti_derivative_sigmoid, False, "sigmoid", [0, 1])
+    test_other_fun(trained_model, np.exp, np.exp, False, "exp", [0, 1])
 
 
-random_fun_coeff = dataset_creation.generate_coeff()
-random_fun = np.poly1d(random_fun_coeff)
-random_fun_integral = np.poly1d(np.polyint(random_fun))
+    random_fun_coeff = dataset_creation.generate_coeff()
+    random_fun = np.poly1d(random_fun_coeff)
+    random_fun_integral = np.poly1d(np.polyint(random_fun))
 
-test_other_fun(trained_model, random_fun, random_fun_integral, False, "random", [0, 1])
-test_other_fun(trained_model, random_fun, random_fun_integral, False, "random", [-2, -1])
+    test_other_fun(trained_model, random_fun, random_fun_integral, False, "random", [0, 1])
+    test_other_fun(trained_model, random_fun, random_fun_integral, False, "random", [-2, -1])
 
 
